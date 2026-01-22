876db4faa53014f0178424c142f6094d02f0216f
Decoy-Devil <122229006+Decoy-Devil@users.noreply.github.com>
2026-01-21 23:40:15 -0800

    adding new prog contains_duplicate_array question leet code

---
+++ src/Leet_Contains_Duplicate.py
@@ -0,0 +1,16 @@
+import sys
+
+nums = [1,2,3,1]
+n = a = b = 0
+
+for i in range(len(nums)):
+    for j in range(len(nums)):
+        # n = 1 + j
+        if nums[i] == nums[j] and i != j :
+            a += 1
+            print("true", nums[i], "=", nums[j])
+            sys.exit()    # // return exits the function not the program
+        else:
+            b += 1
+            print("false", nums[i],  nums[j])
+
