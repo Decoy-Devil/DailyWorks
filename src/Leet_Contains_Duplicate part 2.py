e92618c351c062c2e3972ea614294c5416067a7a
Decoy-Devil <122229006+Decoy-Devil@users.noreply.github.com>
2026-01-23 23:57:42 -0800

    adding duplicate in array code3

---
+++ src/Leet_Contains_Duplicate part 2.py
@@ -0,0 +1,19 @@
+import sys
+
+nums = [1,2,3,1]
+# n =
+a = b = 0
+
+for i in range(len(nums)):
+    for j in range(len(nums)):
+        # n = 1 + j
+        if nums[i] == nums[j] and i != j :
+            a += 1
+            print("true", nums[i], "=", nums[j])
+            sys.exit()    # // return <> exits the function not the program // but leet code doesnt want sys.exit
+        else:
+            b += 1
+            print("false", nums[i],  nums[j])
+
+
+# learning concepts of set
