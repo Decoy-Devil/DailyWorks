500f2043b5f0929a569baa9aad56dde41cc42003
Decoy-Devil <122229006+Decoy-Devil@users.noreply.github.com>
2026-01-25 23:55:13 -0800

    adding valid anagram1 still continued

--- src/Leet_valid_anagram.py
+++ src/Leet_valid_anagram.py
@@ -1,15 +1,24 @@
 # Input: s = "anagram", t = "nagaram"
 # Output: true
 
-s = ["a","n","a","g","r","a","m"]
-t = ["n","a","g","a","r","a","i"]
+# s = ["a","n","a","g","r","a","m"]
+s = ["p","p","o","l"]
+t = ["p","o","o","l"]
+# t = ["n","a","g","a","r","a","t"]
+# t = ["n","a","g","a","r","a"]
 
 # for i in s:
 #     for j in t:
 #
+if (len(s) != len(t)):
+    print("unmatched strings")
+    exit()
+else:
+    for x in s:
+        if x in t:
+            print(x, "true")
+        else:
+            print(x, "not matched")
 
-for x in s:
-    if x in t:
-        print(x, "true")
-    else:
-        print(x, "not matched")
+
+# failing on test case of pool vs ppol, where characters are same but duplicates are different in same length
