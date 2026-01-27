b4e9c4b7149924b11749e5ce13d3ee2ecafbd54b
Decoy-Devil <122229006+Decoy-Devil@users.noreply.github.com>
2026-01-26 23:07:39 -0800

    fixed the valid test case for pool vs ppold

---
+++ src/Leet_valid_anagram 2 test case working.py
@@ -0,0 +1,26 @@
+# Input: s = "anagram", t = "nagaram"
+# Output: true
+
+# s = ["a","n","a","g","r","a","m"]
+s = ["p","p","o","l"]
+t = ["p","o","o","l"]
+# t = ["n","a","g","a","r","a","t"]
+# t = ["n","a","g","a","r","a"]
+
+# for i in s:
+#     for j in t:
+#
+if (len(s) != len(t)):
+    print("unmatched strings")
+    exit()
+else:
+    for x in s:
+        if x in t:
+            t.remove(x)
+            print(x, "true")
+        else:
+            print(x, "not matched")
+
+
+# failing on test case of pool vs ppol,
+# where characters are same but duplicates are different in same length
