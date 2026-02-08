cc8b9ac9188365ecb39ec30db401fe5e78f0eecf
Decoy-Devil <122229006+Decoy-Devil@users.noreply.github.com>
2026-02-07 20:59:13 -0800

    adding the valid anagram

--- README.md
+++ README.md
@@ -9,3 +9,4 @@
 | 2   | <b>Score of a String</b>  <br> | [View](https://github.com/Decoy-Devil/DailyWorks/blob/main/src/Score_of_a_String.py)
 | 3   | <b>Palindrome Number_Real</b>  <br> | [View](https://github.com/Decoy-Devil/DailyWorks/blob/main/src/Palindrome_Number_Real.py)
 | 4   | <b>Roman to Integer</b>  <br> | [View](https://github.com/Decoy-Devil/DailyWorks/blob/main/src/Roman_to_Integer.py)
+| 5   | <b>Valid Anagram</b>  <br> | [View](https://github.com/Decoy-Devil/DailyWorks/blob/main/src/Valid_Anagram.py)
---
+++ src/Valid_Anagram.py
@@ -0,0 +1,58 @@
+"""
+242. Valid Anagram
+
+Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+
+Example 1:
+
+Input: s = "anagram", t = "nagaram"
+
+Output: true
+
+Example 2:
+
+Input: s = "rat", t = "car"
+
+Output: false
+
+Constraints:
+
+1 <= s.length, t.length <= 5 * 104
+s and t consist of lowercase English letters.
+ 
+Follow up: What if the inputs contain Unicode characters? How would you adapt your solution to such a case?
+"""
+
+
+class Solution(object):
+    def isAnagram(self, s , t ):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        if len(s) != len(t):
+            return False
+        
+        dictionary_a = {}
+        dictionary_b = {}
+
+        for i in range(len(s)):
+            # if key not in dictionary_a:
+            #     dictionary_a[key] = 1
+            # else:
+            #     dictionary_a[key] = 1 + dictionary_a[key]
+            # value = dictionary_a[key]
+            dictionary_a[s[i]] = 1 + dictionary_a.get(s[i], 0)
+    # -----
+            # key = t[i]
+            # if key not in dictionary_b:
+            #     dictionary_b[key] = 1
+            # else:
+            #     dictionary_b[key] = 1 + dictionary_b[key]
+            # value = dictionary_b[key]
+            dictionary_b[t[i]] = 1 + dictionary_b.get(t[i], 0)
+        return (dictionary_a == dictionary_b)
+        
+        
+        
